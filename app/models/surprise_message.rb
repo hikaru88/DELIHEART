990e2e2c6abae7a00f5dcd2bbde4da9d8f7cf074
hikaru88 <hikaru8705@gmail.com>
2020-05-30 16:35:29 +0900

    change_spec_order finish

--- app/models/surprise_message.rb
+++
@@ -1,4 +0,0 @@
-class SurpriseMessage < ApplicationRecord
-  belongs_to :surprise
-  belongs_to :user
-end
--- test/models/surprise_message_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SurpriseMessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
